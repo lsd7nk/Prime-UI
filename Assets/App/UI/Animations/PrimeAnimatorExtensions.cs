efb2f589919788f477cc0aab65a34c7f54af8137
lsd7nk <87329383+ElovikovSergei@users.noreply.github.com>
2025-01-26 05:31:34 +0700

    add prime animator

---
+++ Assets/App/UI/Animations/PrimeAnimatorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using PrimeTween;
+using System;
+
+namespace App.UI.Animations
+{
+    public static class PrimeAnimatorExtensions
+    {
+        public static Easing GetEase(this AbstractAnimation animation)
+        {
+            return animation.EaseType switch
+            {
+                EaseType.Ease => animation.Ease,
+                EaseType.AnimationCurve => animation.AnimationCurve,
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+}
