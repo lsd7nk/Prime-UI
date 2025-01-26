42e57308d2c4da5976bd7ad009ed39e5248ed8fc
lsd7nk <87329383+ElovikovSergei@users.noreply.github.com>
2025-01-27 04:25:05 +0700

    add prime container

---
+++ Assets/App/UI/Container/PrimeContainer.cs
@@ -0,0 +1,48 @@
+using App.UI.Animations;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+namespace App.UI.Container
+{
+    [Serializable]
+    public sealed class PrimeContainer : MonoBehaviour
+    {
+        [field: SerializeField] public GraphicRaycaster GraphicRaycaster { get; private set; }
+        [field: SerializeField] public RectTransform RectTransform { get; private set; }
+        [field: SerializeField] public CanvasGroup CanvasGroup { get; private set; }
+
+        public Vector3 StartPosition { get; private set; } = PrimeAnimatorConstants.START_POSITION;
+        public Vector3 StartRotation { get; private set; } = PrimeAnimatorConstants.START_ROTATION;
+        public Vector3 StartScale { get; private set; } = PrimeAnimatorConstants.START_SCALE;
+        public float StartAlpha { get; private set; } = PrimeAnimatorConstants.START_ALPHA;
+
+        public void ResetPosition()
+        {
+            RectTransform.anchoredPosition3D = StartPosition;
+        }
+
+        public void ResetRotation()
+        {
+            RectTransform.localEulerAngles = StartRotation;
+        }
+
+        public void ResetScale()
+        {
+            RectTransform.localScale = StartScale;
+        }
+
+        public void ResetAlpha()
+        {
+            CanvasGroup.alpha = StartAlpha;
+        }
+
+        public void ResetValues()
+        {
+            ResetPosition();
+            ResetRotation();
+            ResetScale();
+            ResetAlpha();
+        }
+    }
+}
