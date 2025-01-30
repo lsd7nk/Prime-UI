cb62bd67ec98be4f9b24273b0afb89fe59338106
lsd7nk <87329383+ElovikovSergei@users.noreply.github.com>
2025-01-30 22:30:02 +0700

    add prime view

---
+++ Assets/App/UI/View/PrimeView.cs
@@ -0,0 +1,11 @@
+using App.UI.Container;
+using UnityEngine;
+
+namespace App.UI.View
+{
+    [RequireComponent(typeof(PrimeContainer))]
+    public sealed class PrimeView : PrimeAnimatedComponent
+    {
+
+    }
+}
