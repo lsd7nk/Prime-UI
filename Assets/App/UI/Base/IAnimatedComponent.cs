aaac34b936d30ddc75c4af70ac94f345beb59442
lsd7nk <87329383+ElovikovSergei@users.noreply.github.com>
2025-02-02 18:47:55 +0700

    to latest commit

--- Assets/App/UI/Base/IAnimatedComponent.cs
+++ Assets/App/UI/Base/IAnimatedComponent.cs
@@ -1,9 +1,16 @@
 using Cysharp.Threading.Tasks;
+using System;
 
 namespace Prime.UI
 {
     public interface IAnimatedComponent
     {
+        public event Action OnShowStartEvent;
+        public event Action OnShowFinishEvent;
+
+        public event Action OnHideStartEvent;
+        public event Action OnHideFinishEvent;
+
         public void Show(bool withoutAnimation = false);
         public void Hide(bool withoutAnimation = false);
 
