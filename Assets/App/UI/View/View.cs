6b393e831b6af5584d9322755a31cd5a33989060
lsd7nk <87329383+lsd7nk@users.noreply.github.com>
2025-02-09 00:06:09 +0700

    view creating

--- Assets/App/UI/View/View.cs
+++ Assets/App/UI/View/View.cs
@@ -1,3 +1,4 @@
+using Prime.UI.Extensions;
 using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
@@ -8,19 +9,32 @@ namespace Prime.UI.View
     [RequireComponent(typeof(Container))]
     public sealed class View : AnimatedComponent
     {
+        public void Initialize()
+        {
+            if (_animatedContainer == null)
+            {
+                _animatedContainer = GetComponent<Container>();
+            }
+        }
+
 #if UNITY_EDITOR
         [MenuItem(PrimeUtils.VIEW_PATH, false, PrimeUtils.COMPONENT_PRIORITY)]
         private static void CreateComponent(MenuCommand menuCommand)
         {
-            //var gameObject = new GameObject(PrimeUtils.VIEW, typeof(RectTransform), typeof(View));
-            //var rootCanvas = gameObject.GetComponent<Canvas>().rootCanvas;
+            var viewObject = new GameObject(PrimeUtils.VIEW, typeof(RectTransform), typeof(View));
+            var parentObject = PrimeUtils.GetCanvasAsParent(menuCommand.context as GameObject);
 
-            //if (rootCanvas != null)
-            //{
-            //    GameObjectUtility.SetParentAndAlign(gameObject, rootCanvas.gameObject);
-            //}
+            GameObjectUtility.SetParentAndAlign(viewObject, parentObject);
+            Undo.RegisterCreatedObjectUndo(viewObject, "Create " + viewObject.name);
 
-            //Undo.RegisterCreatedObjectUndo(gameObject, "Create " + gameObject.name);
+            var viewRectTransform = viewObject.GetComponent<RectTransform>();
+            var container = viewObject.GetComponent<Container>();
+            var view = viewObject.GetComponent<View>();
+
+            viewRectTransform.SetFullScreen(true);
+
+            container.Initialize();
+            view.Initialize();
         }
 #endif
     }
